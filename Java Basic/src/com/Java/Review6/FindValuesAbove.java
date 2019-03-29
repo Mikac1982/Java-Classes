c640ec304ded707e17d1c9e4ab2a67a62b298b7f
Mikac1982 <sibalic.milena@gmail.com>
2019-03-28 20:50:41 -0400

    First code commit

---
+++ Java Basic/src/com/Java/Review6/FindValuesAbove.java
@@ -0,0 +1,39 @@
+package com.Java.Review6;
+
+public class FindValuesAbove {
+   public static void main(String[] args) {
+	
+   // Find the number of values above the value of 7.   
+	  //  2 6 9 3 7
+	  //  1 7 9 4 8
+	  //  0 4 6 2 5
+	  //  0 1 2 3
+	    	 
+	 int[][] rating = { { 2, 6, 9, 3, 7 }, { 1, 7, 9, 4, 8 }, { 0, 4, 6, 2, 5 }, { 0, 1, 2, 3 } };	
+	 
+	 int count=0;
+	 System.out.println("-------For Loop------");
+	 
+	 for (int i=0; i<rating.length; i++)  {
+		 for (int y=0; y<rating[i].length; y++)  {
+			 
+			 if (rating[i][y]>7) {
+				 count+=1;
+			 }
+		 }
+	 }
+	 System.out.println("The number of values above 7 is "+count);
+	 
+	 System.out.println("------For Each loop------");
+	 
+	 for (int[] arrays:rating) {
+		 for (int elements:arrays) {
+			 
+			 if (elements>7) {
+				 count+=1;
+			 }
+		 }
+	 }
+	 System.out.println("The number of elements avove value 7 is "+count);  
+}
+}
